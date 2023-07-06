2fe4582bf762df413858c83326feaf43f2508bf3
boconnor1993 <brendan_oconnor1993@hotmail.com>
2023-07-06 12:24:51 +1000

    Complete playerSelection function
    Completed the playerselection function with a while loop. Tested.

--- psr.js
+++ psr.js
@@ -1,17 +1,30 @@
-const options = ["Rock", "Paper", "Scissors"]
+const options = ["rock", "paper", "scissors"]
+
+let computerItem
 
 function getComputerChoice() {
     return options[Math.floor(Math.random() * 3)]
 }
 
-// console.log(getComputerChoice())
-
 
 function playerSelection() {
-
+    let playerItem;
+    while (true) {
+        playerItem = prompt("Choose an option: rock, paper or scissors: ").toLowerCase().trim();
+        if (options.includes(playerItem)) { break }
+        else { console.log("Please provide a valid input") }
+    }
+    return playerItem
 }
 
 function computerSelection() {
 
 }
 
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game(){
+
+}
