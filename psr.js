706f78f358030656f7b7427ba0f18ed7ca8e8d08
boconnor1993 <brendan_oconnor1993@hotmail.com>
2023-07-06 14:59:56 +1000

    Finish game function and score bug fix
    Completed the implementation of the main game function.
    There was also an issue with scores not incrementing correctly. This
    was fixed with an amendment to the ++ logic.

--- psr.js
+++ psr.js
@@ -2,6 +2,7 @@
 const options = ["rock", "paper", "scissors"];
 const player = "player"
 const computer = "computer"
+const draw = "draw"
 
 function computerSelection() {
     // Run random method to choose one of the items in the array above
@@ -36,6 +37,7 @@ function playRound() {
     // Check if game is a draw and log the draw with no further function calls
     if (playerItem === computerItem) {
         console.log("Draw!")
+        return draw
     }
     // Check all player win conditions, then display the win message and return the winner
     else if (
@@ -46,19 +48,30 @@ function playRound() {
         console.log(`Congrats! You win with ${playerItem} as the computer chose ${computerItem}!`)
         return player
     }
-    else {
-        console.log(`Congrats! You lose with ${playerItem} as the computer chose ${computerItem}!`)
+    // Check all computer win conditions, then display the win message and return the winner
+    else if (
+        (computerItem === "rock" && playerItem === "scissors") || 
+        (computerItem === "scissors" && playerItem === "paper") || 
+        (computerItem === "paper" && playerItem === "rock")
+    ) {
+        console.log(`Unlucky! You lose with ${playerItem} as the computer chose ${computerItem}!`)
         return computer
+    }
+    else {
+        console.log("Error with playRound function, investigate")
     }
 }
 
 function updateScore(winner, playerScore, computerScore) {
     // Find who the winner is and increment their score up and return it.
     if (winner === player) {
-        playerScore = playerScore++
+        playerScore++
     }
     else if (winner === computer) {
-        computerScore = computerScore++
+        computerScore++
+    }
+    else if (winner === draw) {
+        return [playerScore, computerScore]
     }
     else {
         console.log("Error with updateScore function, investigate")
@@ -67,18 +80,31 @@ function updateScore(winner, playerScore, computerScore) {
 }
 
 function game(){
-    const winScore = 0;
+    // Initialise game specific variables
+    const winScore = 3;
     let playerScore = 0;
     let computerScore = 0;
 
-    while (playerScore < winScore || computerScore < winScore) {
+    // Run a loop until either player or computer wins
+    while (playerScore < winScore && computerScore < winScore) {
+        // Play a round and then update score with returned winner variable
         let winner = playRound()
+        let scores = updateScore(winner, playerScore, computerScore)
+        playerScore = scores[0]
+        computerScore = scores[1]
+        showScore(playerScore, computerScore)
     }
 
+    // Once broken out of loop, display final scores
     if (playerScore === winScore) {
         console.log(`Congratulations! You have won ${playerScore} to ${computerScore}!`)
     }
     else if (computerScore === winScore) {
         console.log(`Unlucky! You have lost ${playerScore} to ${computerScore}!`)
     }
+    else {
+        console.log("Error with game function, investigate")
+    }
 }
+
+game()
