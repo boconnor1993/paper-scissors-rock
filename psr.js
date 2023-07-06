3fd0a3333ff691cfb60872213f4a518c59114941
boconnor1993 <brendan_oconnor1993@hotmail.com>
2023-07-06 11:09:06 +1000

    getComputerChoice added
    Added random computer choice functionality

--- psr.js
+++ psr.js
@@ -1,6 +1,11 @@
-function getComputerChoice() {
+const options = ["Rock", "Paper", "Scissors"]
 
+function getComputerChoice() {
+    return options[Math.floor(Math.random() * 3)]
 }
+
+// console.log(getComputerChoice())
+
 
 function playerSelection() {
 
